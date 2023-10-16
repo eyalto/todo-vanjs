ba6764c5db586b57598990cfe41cd6328e69d0d6
Eyal Toledano <eyal.toledano@gmail.com>
2023-10-16 11:25:18 +0300

    initial repo

---
+++ src/todo.js
@@ -0,0 +1,23 @@
+import van from "https://cdn.jsdelivr.net/gh/vanjs-org/van/public/van-1.2.1.min.js"
+
+const {a, button, div, input, span, strike} = van.tags
+
+const TodoItem = ({text}) => {
+  const done = van.state(false), deleted = van.state(false)
+  return () => deleted.val ? null : div(
+    input({type: "checkbox", checked: done, onclick: e => done.val = e.target.checked}),
+    () => (done.val ? strike : span)(text),
+    a({onclick: () => deleted.val = true}, "❌"),
+  )
+}
+
+const TodoList = () => {
+  const inputDom = input({type: "text"})
+  const dom = div(
+    inputDom,
+    button({onclick: () => van.add(dom, TodoItem({text: inputDom.value}))}, "Add"),
+  )
+  return dom
+}
+
+van.add(document.body, TodoList())
